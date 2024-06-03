b265b1edfab4079765aa0dc826a6bfffbf14604c
mmh-masud03 <2054901003@student.bup.edu.bd>
2024-06-03 23:52:13 +0600

    ts removed

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,16 +1,21 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { getServerSession } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { prisma } from "./connect";
+import prisma from "./connect";
+import { getServerSession } from "next-auth";
+
 export const authOptions = {
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
-  ]
-}
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
 
-export const getAuthSession = ()=> getServerSession(authOptions)
+export const getAuthSession = () => getServerSession(authOptions);
